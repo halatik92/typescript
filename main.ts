1a55ce25aaa78a97c4904e9a40966dee7b9212cb
Maxim <maxcadet92@gmail.com>
2022-03-11 16:18:09 +0500

    complite

--- main.ts
+++ main.ts
@@ -1,13 +1,22 @@
 // возвращает сумму элементов массива
 export function arraySum(arr: number[]): number {
+  let sum: number = 0
+  return  arr.reduce((acc: number = 0, el) => acc + el, sum)
 }
 
 // возвращает исходную строку в Title Case
-export function toTitleCase(str: string[]): string {
+export function toTitleCase(str: string): string {
+  return str.toUpperCase()
 }
 
 // возвращает Promise, который зарезолвит true, если b больше чем a, и зареджектит, если наоборот
 export function promiseCompare(a: number, b: number): Promise<true> {
+  return new Promise((resolve, reject) => {
+    if (b > a) {
+      resolve(true)
+    }
+    reject(false);
+  });
 }
 
 export async function asyncCompare(): Promise<true> {
@@ -20,7 +29,7 @@ export async function asyncCompare(): Promise<true> {
       res = promiseCompare(a, b);
     }
     catch (e) {
-      b--;
+      b++;
     }
   }
 
